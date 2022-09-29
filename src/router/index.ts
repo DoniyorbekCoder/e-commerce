260262193569a760d803c4a58aaf4aa2a78f243d
Doniyorbek Asqaraliyev <dcoder0404@gmail.com>
2022-09-29 11:16:21 +0500

    deploy bug fix

--- src/router/index.ts
+++ src/router/index.ts
@@ -9,21 +9,21 @@ const router = createRouter({
       name: "home",
       component: HomeView,
     },
-    {
-      path: "/401",
-      name: "unauthentication",
-      component: () => import("@/views/errorViews/unAuthentication.vue"),
-    },
-    {
-      path: "/404",
-      name: "page_not_found",
-      component: () => import("@/views/errorViews/pageNotFound.vue"),
-    },
-    {
-      path: "/500",
-      name: "server_error",
-      component: () => import("@/views/errorViews/serverError.vue"),
-    },
+    // {
+    //   path: "/401",
+    //   name: "unauthentication",
+    //   component: () => import("@/views/errorViews/unAuthentication.vue"),
+    // },
+    // {
+    //   path: "/404",
+    //   name: "page_not_found",
+    //   component: () => import("@/views/errorViews/pageNotFound.vue"),
+    // },
+    // {
+    //   path: "/500",
+    //   name: "server_error",
+    //   component: () => import("@/views/errorViews/serverError.vue"),
+    // },
     {
       path: "/login",
       name: "login",
