92e06c29c1b4e479d242f219c9e92b79012635de
Doniyorbek Asqaraliyev <dcoder0404@gmail.com>
2022-09-29 19:37:41 +0500

    fix bug

--- dist/assets/index.8435d1b9.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o,c,a as s}from"./index.737cfd38.js";const t={},n=s("h1",null,"Warehouse page",-1),a=[n];function r(_,d){return o(),c("div",null,a)}const i=e(t,[["render",r]]);export{i as default};
--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,48 +1,54 @@
-import axios from 'axios'
-import router from '@/router'
+import axios from "axios";
+import router from "@/router";
 
 export const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL as string,
   timeout: 20000,
   headers: {
-    'Content-Type': 'application/json',
-  }
+    "Content-Type": "application/json",
+  },
 });
 
 axiosInstance.interceptors.request.use(
-  config => {
-    const token = sessionStorage.getItem('token')
+  (config) => {
+    const token = sessionStorage.getItem("token");
     config.headers = {
-      'Authorization': `Bearer ` + token
+      Authorization: `Bearer ` + token,
       // 'Authorization': `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbiIsImlzcyI6ImFkbWluLmV2bzI0LnV6IiwiZXhwIjoxNjY2NjA0ODA3LCJ1dWlkIjoiMTE0NTk0ZWEtYzBmNC00OGE2LWJiYjYtMjQ5YThhNzE2Y2RjLTIifQ.Pkg4eVmxf83tLvqC9KfQM-jaEVEZ-BoabbMS7f_cC6a6Z1TW6HY40R74DJ-aeZuTYZngSichEiztz6pVfOYb5A`
-    }
+    };
 
-    return config
+    return config;
   },
-  error => {
-    return Promise.reject(error)
+  (error) => {
+    return Promise.reject(error);
   }
-)
-axiosInstance.interceptors.response.use((response) => {
-  return response;
-}, (error) => {
-  if (error.request.status === 401) {
-    router.push('/401');
-  } else if (error.request.status === 404) {
-    router.push('/404');
-  } else if (error.request.status === 500) {
-    router.push('/500')
-  } else return Promise.reject(error);
-})
+);
+axiosInstance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.request.status === 401) {
+      router.push("/401");
+    } else if (error.request.status === 404) {
+      router.push("/404");
+    } else if (error.request.status === 500) {
+      router.push("/500");
+    } else return Promise.reject(error);
+  }
+);
 
-axiosInstance.interceptors.request.use((response) => {
-  return response;
-}, (error) => {
-  if (error.request.status === 401) {
-    router.push('/401');
-  } else if (error.request.status === 404) {
-    router.push('/404');
-  } else if (error.request.status === 500) {
-    router.push('/500')
-  } else return Promise.reject(error);
-})
+axiosInstance.interceptors.request.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.request.status === 401) {
+      router.push("/401");
+    } else if (error.request.status === 404) {
+      router.push("/404");
+    } else if (error.request.status === 500) {
+      router.push("/500");
+    } else return Promise.reject(error);
+  }
+);
